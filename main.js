a82da6cc6fad340507f76080c78e5bf3b6defba9
myshathakkar <78802180+myshathakkar@users.noreply.github.com>
2021-09-08 18:58:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+width:300,
+heigth:350,
+image_format:"png",
+png_quality:90
+});
+camera=document.getElementById("camera")
+Webcam.attach(camera)
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById('result').innerHTML="<img id='captured_image'src='"+data_uri+"'>"
+    })
+}
+console.log("ml5version",ml5.version)
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XreoRD2A1/model.json",model_loaded)
+function model_loaded(){
+    console.log('model loaded')
+}
+function check(){
+    img=document.getElementById("captured_image")
+    classifier.classify(img,got_result)
+}
+function got_result(error,result){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(result)
+    document.getElementById("result_object_name").innerHTML=result[0].label
+    document.getElementById("result_object_accuracy").innerHTML=result[0].confidence.toFixed(3)
+}
+}
